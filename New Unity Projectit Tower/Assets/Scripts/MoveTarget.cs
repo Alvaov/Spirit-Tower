4470f1cca02c44a06fc93d4699ae8864483d69c9
Brian Wagemans <briwag88@hotmail.com>
2020-06-19 14:08:13 -0600

    Camara en el grid creado por yendry

--- New Unity Projectit Tower/Assets/Scripts/MoveTarget.cs
+++ New Unity Projectit Tower/Assets/Scripts/MoveTarget.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class MoveTarget : MonoBehaviour
 {
+    private Vector3 movement;
+    private float horizontal;
+    private float vertical;
+    private float speed = 10;
+    public CharacterController player;
+
     [SerializeField]
     Transform[] wayPoints;
     int currentWayPoint = 0;
@@ -13,13 +19,17 @@ public class MoveTarget : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        movement = new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime;
+        movement = Vector3.ClampMagnitude(movement, 1);
+        player.Move(movement);
     }
 
 }
