07e596bd1170977d55c610de7e101628197ede43
Yen Badilla <yendrypamela@hotmail.com>
2020-06-19 11:23:30 -0600

    Grid Scripts

---
+++ New Unity Projectit Tower/Assets/Scripts/MoveTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTarget : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] wayPoints;
+    int currentWayPoint = 0;
+    Rigidbody rigidbody;
+    [SerializeField]
+    float moveSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
