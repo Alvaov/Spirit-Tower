e9a7be42b76a4a3c271474516e849126c83fc7c6
Steven <stevencm199gmail.com>
2020-06-23 12:27:34 -0600

    Auto stash before merge of "Steven" and "origin/Vasquez"

--- Spirit Tower/Assets/Scripts/PlayerControllerTransform.cs
+++ Spirit Tower/Assets/Scripts/PlayerControllerTransform.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using UnityEngine;
 
 public class PlayerControllerTransform : MonoBehaviour{
-    private Vector3 movement;
-    public float horizontal;
-    public float vertical;
-    public float gravity = -9.8f;
-    public float speed = 10;
+
+    
+    public float rightInput;
+    public float forwardInput;
+    
     public CharacterController player;
+    public CameraFollow camera;
+
+    private Vector3 velocity; 
+
+    private Vector3 movement;
+    public float speed = 10;
     public Animator animator;
+    public float gravity = -9.8f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,11 +34,24 @@ public class PlayerControllerTransform : MonoBehaviour{
     {
         if (player.isGrounded)
         {
-            horizontal = Input.GetAxis("Horizontal");
-            vertical = Input.GetAxis("Vertical");
+           
+            rightInput = Input.GetAxis("Horizontal");
+            forwardInput = Input.GetAxis("Vertical");
+
+
+            /*Vector3 camFwd = camera.transform.forward;
+            Vector3 camRight = camera.transform.right;
+
+            Vector3 targetLocation = vertical * camera.transform.forward;
+            targetLocation += horizontal * camera.transform.right;
+
+            Vector3 velocity = (targetLocation - transform.position).normalized;*/
+
+
+
             animator.SetFloat("vertical", Input.GetAxis("Vertical"));
             animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
-            movement = new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime;
+            movement = new Vector3(rightInput, 0f, forwardInput) * speed * Time.deltaTime;
             movement = Vector3.ClampMagnitude(movement, 1);
             player.Move(movement);
         }
@@ -38,4 +61,36 @@ public class PlayerControllerTransform : MonoBehaviour{
             player.Move(movement);
         }
     }
+    
+
+   /* private void Update()
+    {
+        transform.Translate(velocity);
+    }*/
+
+
+
+  /*  void MovementInput(float forward, float right)
+    {
+        forwardInput = forward;
+        rightInput = right;
+
+
+        Vector3 camFwd = camera.transform.forward;
+        Vector3 camRight = camera.transform.right;
+
+        Vector3 targetLocation = forward * camera.transform.forward;
+        targetLocation += right * camera.transform.right;
+
+        velocity = (targetLocation - transform.position).normalized;
+    }*/
+
+
+    
+
+
+
 }
+
+
+
--- Spirit Tower/Assets/Scripts/UIManager.cs
+++ Spirit Tower/Assets/Scripts/UIManager.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
     public GameObject startMenu;
     public InputField userNameField;
+
+    public GameObject luz;
+    public Text titulo;
+
 
     public void Awake()
     {
@@ -25,7 +31,10 @@ public class UIManager : MonoBehaviour
     public void ConnectToServer()
     {
         startMenu.SetActive(false);
+        titulo.gameObject.SetActive(false);
         userNameField.interactable = false;
+        luz.SetActive (false);
+        SceneManager.LoadScene("Floor", LoadSceneMode.Additive);
         Client.instance.ConnectToServer();
         Client.instance.tcp.SendData("k:l:l:");
     }
