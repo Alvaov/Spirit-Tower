587d2d10860f31b08edc4823a9d3fb662d53996f
Steven <stevencm199gmail.com>
2020-06-23 12:11:35 -0600

    Camara inteligente

---
+++ Spirit Tower/Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Transactions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+
+    private int health = 5;
+    public Text healthText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        healthText.text = "Salud: " + health;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            health--;
+            string msg = "Salud restante: " + health;
+            Client.instance.Send_Data(msg.Length + msg);
+        }
+
+    }
+}
