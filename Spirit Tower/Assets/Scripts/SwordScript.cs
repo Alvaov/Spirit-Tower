3cbc6e1f280031a5659f7379e0b44843a50e704e
Alvaov <alvaov@estudiantec.cr>
2020-07-08 14:26:21 -0600

    Espadas y script de espada al jugador

---
+++ Spirit Tower/Assets/Scripts/SwordScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordScript : MonoBehaviour
+{
+    public GameObject rightHand;
+    public bool handed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        handed = false;
+        rightHand = GameObject.FindGameObjectWithTag("RightHand");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            handed = true;
+        }
+    }
+
+    void Update()
+    {
+        if (handed)
+        {
+            transform.parent = rightHand.transform;
+            transform.localPosition = new Vector3(0, 0, 0);
+        }
+    }
+}
