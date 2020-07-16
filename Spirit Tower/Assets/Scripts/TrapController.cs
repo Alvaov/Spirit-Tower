24a3ee125557335eb006b7d05eae57192cb8a522
Yendry Badilla <yen.badilla@gmail.com>
2020-07-16 00:11:58 -0600

    Trampas

---
+++ Spirit Tower/Assets/Scripts/TrapController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("active", true);
+            Client.instance.tcp.SendData("0:Trap:::");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("active", false);
+        }
+    }
+}
